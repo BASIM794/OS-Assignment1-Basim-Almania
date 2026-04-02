c408286f385c02276abfd9d5bcee8694cf2015ae
BASIM794 <445050295@std.psau.edu.sa>
2026-04-02 18:18:30 +0300

    Feature 1: Integrated process priority into scheduling simulation

--- SchedulerSimulation.java
+++ SchedulerSimulation.java
@@ -29,13 +29,17 @@ class Process implements Runnable {
     private int burstTime; // Total time the process requires to complete (in milliseconds)
     private int timeQuantum; // Time slice (time quantum) allowed per CPU access (in milliseconds)
     private int remainingTime; // Time left for the process to finish its execution
-
+    
+    private int priority; // Feature 1 Added priority field to represent process importance
     // Constructor to initialize the process with name, burst time, and time quantum
-    public Process(String name, int burstTime, int timeQuantum) {
+    public Process(String name, int burstTime, int timeQuantum, int priority) {
         this.name = name;
         this.burstTime = burstTime;
         this.timeQuantum = timeQuantum;
         this.remainingTime = burstTime; // Initially, remaining time is equal to the burst time
+        // Feature 1
+        this.priority = priority;
+
     }
 
     // This method will be called when the thread for this process is started
@@ -135,7 +139,14 @@ class Process implements Runnable {
 
     public int getRemainingTime() {
         return remainingTime;
+        
     }
+    // Feature 1
+   // Getter method for process priority
+   public int getPriority() {
+    return priority;
+    }
+
 
     // Check if the process has finished (i.e., no remaining time)
     public boolean isFinished() {
@@ -197,8 +208,13 @@ public class SchedulerSimulation {
             int burstTime = timeQuantum/2 + random.nextInt(2 * timeQuantum + 1);
             
             // Create a new process object with a unique name, burst time, and the defined time quantum
-            Process process = new Process("P" + i, burstTime, timeQuantum);
-            
+            String name = "P" + i;
+            int priority = random.nextInt(5) + 1;
+
+            // Feature 1
+            // Creating process with random priority
+            Process process = new Process(name, burstTime, timeQuantum, priority);
+
             // Add the process to the ready queue and the map
             addProcessToQueue(process, processQueue, processMap);
         }
